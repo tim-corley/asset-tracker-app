af7bfe9f8b56f63db05c3f9536073848e698c1a8
Tim Corley <tcorley215@gmail.com>
2020-03-05 19:49:37 -0500

    Update Aircraft & AircraftList styling

--- src/Aircraft.js
+++ src/Aircraft.js
@@ -4,7 +4,7 @@ import AircraftList from './AircraftList';
 class Aircraft extends Component {
   render() {
     return (
-      <div className="w-3/4 m-auto mb-8 p-10 bg-gray-100 relative rounded shadow">
+      <div className="w-5/6 m-auto mb-8 py-10 px-4 bg-gray-100 relative rounded shadow">
         <hr className="block mb-6 mt-8 w-8 border-blue-600 h-0 border-t-2" />
         <h2 className="z-10 relative text-2xl text-gray-900 absolute font-bold">
           MY AIRCRAFT
@@ -15,7 +15,7 @@ class Aircraft extends Component {
         ) : (
           <div className="font-thin text-xl">
             There is currently no aircraft associated to your account. Please
-            contact an admin in order to have your account linked with your
+            contact an admin in order to have this account linked with your
             aircraft.
           </div>
         )}
--- src/AircraftList.js
+++ src/AircraftList.js
@@ -5,29 +5,46 @@ class AircraftList extends Component {
     const { aircraft } = this.props;
     const myAircraft = aircraft.map(item => {
       return (
-        <div key={item.aircraftRegistration}>
-          <table className="table-auto">
-            <thead>
-              <tr>
-                <th className="px-4 py-2">YEAR</th>
-                <th className="px-4 py-2">MODEL</th>
-                <th className="px-4 py-2">REGISTRATION</th>
-                <th className="px-4 py-2">HOURS</th>
-                <th className="px-4 py-2">CURRENT STATUS</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td className="border px-4 py-2">{item.aircraftYear}</td>
-                <td className="border px-4 py-2">{item.aircraftModel}</td>
-                <td className="border px-4 py-2">
-                  {item.aircraftRegistration}
-                </td>
-                <td className="border px-4 py-2">{item.aircraftHours}</td>
-                <td className="border px-4 py-2">{item.aircraftStatus}</td>
-              </tr>
-            </tbody>
-          </table>
+        <div
+          key={item.aircraftRegistration}
+          className="flex items-center justify-center"
+        >
+          <div className="container">
+            <table className="w-full flex flex-row flex-no-wrap sm:bg-white md:rounded-lg crounded-none overflow-hidden sm:shadow-lg my-5">
+              <thead className="text-left text-white">
+                <tr className="bg-blue-600 flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                  <th className="border border-blue-600 px-4 py-2">YEAR</th>
+                  <th className="border border-blue-600 px-4 py-2">MODEL</th>
+                  <th className="border border-blue-600 px-4 py-2">
+                    REGISTRATION
+                  </th>
+                  <th className="border border-blue-600 px-4 py-2">HOURS</th>
+                  <th className="border border-blue-600 px-4 py-2">
+                    CURRENT STATUS
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="flex-1 sm:flex-none">
+                <tr className="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+                  <td className="border-grey-light border px-4 py-2">
+                    {item.aircraftYear}
+                  </td>
+                  <td className="border-grey-light border px-4 py-2">
+                    {item.aircraftModel}
+                  </td>
+                  <td className="border-grey-light border px-4 py-2">
+                    {item.aircraftRegistration}
+                  </td>
+                  <td className="border-grey-light border px-4 py-2">
+                    {item.aircraftHours}
+                  </td>
+                  <td className="border-grey-light border px-4 py-2">
+                    {item.aircraftStatus}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       );
     });
