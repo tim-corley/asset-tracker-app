cb4c460377675d6b563bf7f62ef4084c5115948a
Tim Corley <tcorley215@gmail.com>
2020-03-05 19:47:52 -0500

    Add Navigation & Homepage components

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
+import { GiAirplane } from 'react-icons/gi';
+import { Link } from '@reach/router';
 
 class Home extends Component {
   render() {
+    const { user } = this.props;
     return (
       <section className="w-3/6 m-auto flex flex-wrap mt-20">
         <div className="flex flex-wrap">
           <div className="w-full mb-8 p-10 bg-gray-100 relative rounded shadow">
-            <span className="text-gray-300 text-6xl absolute italic right-10">
-              01
+            <hr className="block mb-6 mt-8 w-8 border-blue-600 h-0 border-t-2" />
+            <span className="text-gray-300 text-6xl absolute italic right-10 mt-6">
+              <GiAirplane />
             </span>
-            <h2 className="z-10 relative text-2xl text-gray-900 absolute">
-              TAILWIND CSS DEMO
+            <h2 className="z-10 relative text-2xl text-gray-900 absolute font-bold">
+              ASSET TRACKER
             </h2>
-            <hr className="block mb-6 mt-5 w-8 border-red-600 h-0 border-t-2" />
-            <p className="text-gray-800">
-              Ex Lorem eu voluptate eiusmod culpa aliqua exercitation irure
-              minim cillum sit. Incididunt eu aliqua sint eiusmod ex Lorem
-              deserunt et veniam adipisicing ea qui. Officia amet dolor sint
-              commodo ad est ut reprehenderit sit laboris aute minim et labore.
-              Laboris est elit ex ex proident do pariatur dolor non. Consequat
-              anim exercitation enim duis consectetur fugiat eu culpa. Occaecat
-              sunt ex commodo do magna.
+            <hr className="block mb-6 mt-10 w-8 border-blue-600 h-0 border-t-2" />
+            <p className="text-gray-800 mb-10">
+              Welcome to the Asset Tracker! Login or Register to view the
+              real-time status of your asset(s).
             </p>
+            {user == null && (
+              <span>
+                <Link
+                  to="/login"
+                  className="bg-transparent hover:bg-blue-600 text-blue-700 font-semibold hover:text-white py-2 px-4 mr-5 border border-blue-500 hover:border-transparent rounded"
+                >
+                  Log In
+                </Link>
+                <Link
+                  to="/register"
+                  className="bg-transparent hover:bg-blue-600 text-blue-700 font-semibold hover:text-white py-2 px-4 ml-5 border border-blue-500 hover:border-transparent rounded"
+                >
+                  Register
+                </Link>
+              </span>
+            )}
+            {user && (
+              <Link
+                to="/assets"
+                className="bg-transparent hover:bg-blue-600 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+              >
+                Assets
+              </Link>
+            )}
           </div>
         </div>
       </section>
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,11 +1,75 @@
 import React, { Component } from 'react';
+import { GiTargeting } from 'react-icons/gi';
+import { Link } from '@reach/router';
 
 class Navigation extends Component {
   render() {
+    const { user, logoutUser } = this.props;
     return (
-      <section className="w-3/6 m-auto flex flex-wrap mt-20">
-        <div className="flex flex-wrap"></div>
-      </section>
+      <nav className="flex items-center justify-between flex-wrap bg-blue-600 p-6">
+        <Link
+          to="/"
+          className="flex items-center flex-shrink-0 text-white mr-6"
+        >
+          <span className="text-3xl pr-2">
+            <GiTargeting />
+          </span>
+          <span className="font-semibold text-xl tracking-tight">
+            Asset Tracker
+          </span>
+        </Link>
+        <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            {!user && (
+              <Link
+                to="/login"
+                className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-blue-900 mr-4"
+              >
+                Login
+              </Link>
+            )}
+            {!user && (
+              <Link
+                to="/register"
+                className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-blue-900 mr-4"
+              >
+                Register
+              </Link>
+            )}
+            {user && (
+              <Link
+                to="/assets"
+                className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-blue-900 mr-4"
+              >
+                Assets
+              </Link>
+            )}
+          </div>
+          <div>
+            {user && (
+              <Link
+                to="/login"
+                className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-blue-900 mr-4"
+                onClick={e => logoutUser(e)}
+              >
+                Logout
+              </Link>
+            )}
+          </div>
+        </div>
+      </nav>
     );
   }
 }
